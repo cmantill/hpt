78880ea160af8dbe1067770605f9bfc8d291ee4e
gabihamilton <gabihamilton@gmail.com>
2024-08-26 21:03:00 -0500

    adding LHE variables

--- src/hpt/processors/ptSkimmer.py
+++ src/hpt/processors/ptSkimmer.py
@@ -129,6 +129,11 @@ class ptSkimmer(SkimmerABC):
                 vars_dict = gen_selection_dict[d](events, ak4_jets, fatjets, selection_args, P4)
                 genVars = {**genVars, **vars_dict}
 
+        # Add LHE_HT and LHE_Vpt to genVars
+        genVars["LHE_HT"] = events.LHE.HT.to_numpy()
+        genVars["LHE_Vpt"] = events.LHE.Vpt.to_numpy()
+
+
         # used for normalization to cross section below
         gen_selected = (
             selection.all(*selection.names)
