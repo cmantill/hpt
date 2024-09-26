e63851f6553ae6b1c364bc5127245a0b0f105829
Gabi Hamilton <gabihamilton@gmail.com>
2024-09-26 11:58:39 -0400

    Update GenSelection.py

--- src/hpt/processors/GenSelection.py
+++ src/hpt/processors/GenSelection.py
@@ -211,11 +211,11 @@ def gen_selection_V(
 
 
     #GenVVars["GenVis_bb"] = ak.all( vs_pdgId == b_PDGID , axis=1).to_numpy()
-    GenVVars["GenVis_bb"] = ((vs_pdgId[0] == b_PDGID) & (vs_pdgId[1] == b_PDGID)).to_numpy()
+    vs_flat["is_bb"]= ((vs_pdgId[0] == b_PDGID) & (vs_pdgId[1] == b_PDGID))
     #vs_flat["is_cc"] = ak.all( vs_pdgId == c_PDGID , axis=1) 
     vs_flat["is_cs"] = (vs_pdgId[0] == c_PDGID) & (vs_pdgId[1] == s_PDGID)
 
-    #GenVVars["GenVis_bb"] = vs_flat["is_bb"].to_numpy()
+    GenVVars["GenVis_bb"] = vs_flat["is_bb"].to_numpy()
     #GenVVars["GenVis_cc"] = vs_flat["is_cc"].to_numpy()
     GenVVars["GenVis_cs"] = vs_flat["is_cs"].to_numpy()
     
